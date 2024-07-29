31e10211ff81520d5eb4fc056baea5b731787982
Mark Padilha <markpadilha10@gmail.com>
2024-07-29 14:52:39 -0300

    Criada a classe Posicao

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Posicao p = new Posicao(2, 6);
+		System.out.println(p);
+	}
+
+}
